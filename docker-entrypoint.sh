b1d9036d89f33cc2df587d5b3fa43904c2560e49
RuntimeRacer <49956579+RuntimeRacer@users.noreply.github.com>
2023-08-08 21:05:05 +0200

    cleanup debug code; formatting

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,16 +11,16 @@ privateKey=""
 
 #payload folder
 if [[ ! -d /payload ]]; then
-	mkdir /payload
-	if [[ ! -z "$privateKey" ]]; then
-	  echo "$privateKey" > /payload/id_rsa
-	fi
+  mkdir /payload
+  if [[ ! -z "$privateKey" ]]; then
+    echo "$privateKey" >/payload/id_rsa
+  fi
 fi
 
 if [[ ! -d /root/.ssh ]]; then
-	mkdir /root/.ssh
-	chmod 600 /root/.ssh
-	cat > /root/.ssh/config <<-ConfigHD
+  mkdir /root/.ssh
+  chmod 600 /root/.ssh
+  cat >/root/.ssh/config <<-ConfigHD
 	Host    *
 	        UserKnownHostsFile        /dev/null
 	        StrictHostKeyChecking     no
@@ -28,55 +28,51 @@ if [[ ! -d /root/.ssh ]]; then
 	        ServerAliveInterval       5
 	        ServerAliveCountMax       3
 	ConfigHD
-	chmod 600 /root/.ssh/config
-	chmod 600 /root/.ssh/id_rsa
+  chmod 600 /root/.ssh/config
+  chmod 600 /root/.ssh/id_rsa
 fi
 
-
 while true; do
-	OK_TO_LAUNCH=false
-	MOVE_PAYLOAD_FILES=false
-	PLEASE_LOAD_THE_FILES=false
-	# IF the pub and priv keys exist, we are ok to launch
-	if [[ -e ${KEY_LOCATION}/${PRIV_KEY} ]] ; then
-		OK_TO_LAUNCH=true
-	# Check if files are in payload directory
-	elif [[ -e /payload/${PRIV_KEY} ]] ; then
-		#move files to final location and change permisions
-		MOVE_PAYLOAD_FILES=true
-	else
-		PLEASE_LOAD_THE_FILES=true
-	fi
+  OK_TO_LAUNCH=false
+  MOVE_PAYLOAD_FILES=false
+  PLEASE_LOAD_THE_FILES=false
+  # IF the pub and priv keys exist, we are ok to launch
+  if [[ -e ${KEY_LOCATION}/${PRIV_KEY} ]]; then
+    OK_TO_LAUNCH=true
+  # Check if files are in payload directory
+  elif [[ -e /payload/${PRIV_KEY} ]]; then
+    #move files to final location and change permisions
+    MOVE_PAYLOAD_FILES=true
+  else
+    PLEASE_LOAD_THE_FILES=true
+  fi
 
+  if ${OK_TO_LAUNCH}; then
+    echo "Everything OK. Launching."
 
-	if ${OK_TO_LAUNCH}; then
-		echo "Everything OK. Launching."
-
-		ssh dxvpn@5.230.75.110
-
-		echo autossh $@ -N
+    echo autossh $@ -N
     exec autossh $@ -N &
 
     WAIT=$!
 
-		echo /socks5
-		exec /socks5 &
+    echo /socks5
+    exec /socks5 &
 
-		wait $WAIT
+    wait $WAIT
 
-		kill 0
+    kill 0
 
-	fi
+  fi
 
-	if ${MOVE_PAYLOAD_FILES}; then
-		echo "Moving keys..."
-		mv /payload/${PRIV_KEY} ${KEY_LOCATION}/
-		chmod 600 ${KEY_LOCATION}/${PRIV_KEY}
-	fi
+  if ${MOVE_PAYLOAD_FILES}; then
+    echo "Moving keys..."
+    mv /payload/${PRIV_KEY} ${KEY_LOCATION}/
+    chmod 600 ${KEY_LOCATION}/${PRIV_KEY}
+  fi
 
-	if ${PLEASE_LOAD_THE_FILES}; then
-		echo "Please copy key files to container's /payload folder"
-		echo "i.e.: docker cp id_rsa this_container:/payload/"
-		sleep 10
-	fi
+  if ${PLEASE_LOAD_THE_FILES}; then
+    echo "Please copy key files to container's /payload folder"
+    echo "i.e.: docker cp id_rsa this_container:/payload/"
+    sleep 10
+  fi
 done
